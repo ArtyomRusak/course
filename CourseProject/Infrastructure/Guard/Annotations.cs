3aa299c00c8970472f8a3e2126bc9c2766c5fd46
ArtyomRusak <bembi1204@gmail.com>
2013-11-16 17:47:21 +0300

    [Update]: Adding Guard functionality. [ArtyomRusak]

---
+++ CourseProject/Infrastructure/Guard/Annotations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Guard
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+    public sealed class InvokerParameterNameAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.Delegate | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public sealed class NotNullAttribute : Attribute { }
+}
